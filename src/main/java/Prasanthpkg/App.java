6aeac18fe3e848d524fda3182007fab700f9ed5b
prasanthvenna <emailprasanthvenna56@gmail.com>
2022-10-25 12:06:45 +0530

    commits updated

---
+++ src/main/java/Prasanthpkg/App.java
@@ -0,0 +1,13 @@
+package Prasanthpkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
